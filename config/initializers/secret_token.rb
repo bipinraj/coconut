1989e2af6183e5823ab387a4c6bb18de913c55af
Bipin Raj <bipinraj@gmail.com>
2013-01-22 13:14:52 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coconut::Application.config.secret_token = '9a567c0a816e50bc6c6bd921c2fa1992ee5919537ee4eda036db985086edddd93001f1e407df4e48fb588e5d5f5d0efad9d9840ef62f8040488aed7a76ae1402'
